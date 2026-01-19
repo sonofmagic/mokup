c5f749ae0ed875e93a50ebeb3b58b22dc5e9c980
ice breaker <1324318532@qq.com>
2026-01-19 15:20:43 +0800

    docs: update

---
+++ apps/docs/docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { withBase } from 'vitepress'
+import DefaultTheme from 'vitepress/theme'
+import BrandLogo from './components/BrandLogo.vue'
+</script>
+
+<template>
+  <DefaultTheme.Layout>
+    <template #nav-bar-title>
+      <a class="mokup-brand-link" :href="withBase('/')">
+        <BrandLogo size="small" />
+      </a>
+    </template>
+    <template #home-hero-image>
+      <div class="mokup-hero-logo">
+        <BrandLogo size="large" :showText="false" />
+      </div>
+    </template>
+  </DefaultTheme.Layout>
+</template>
---
+++ apps/docs/docs/.vitepress/theme/components/BrandLogo.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+type LogoSize = 'small' | 'large'
+
+withDefaults(
+  defineProps<{
+    size?: LogoSize
+    showText?: boolean
+  }>(),
+  {
+    size: 'small',
+    showText: true,
+  },
+)
+</script>
+
+<template>
+  <span class="mokup-brand" :class="`mokup-brand--${size}`">
+    <img class="mokup-brand__logo" src="/brand/mokup-logo.svg" alt="Mokup mascot">
+    <span v-if="showText" class="mokup-brand__text">Mokup</span>
+  </span>
+</template>
